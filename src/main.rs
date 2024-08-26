38fdbe0341932ffd7fb32d7986ba708eb5906c95
random-sir <antoni.torres@outlook.com>
2024-08-26 14:00:51 -0300

    Removed more old code

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,6 @@ fn main() {
     let mut args: Vec<String> = env::args().collect();
     let raw_path = &mut args[1];
 
-    // let mut chars = raw_path.chars();
-    // // let a = chars.position(|c| c == '(');
-    // let b = chars.position(|c| c == ')');
-
     let mut chars = raw_path.chars();
     let mut path: Vec<String> = vec![String::with_capacity(raw_path.capacity())];
 
